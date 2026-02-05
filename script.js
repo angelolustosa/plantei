a9ecce5bdeac5e0eb74b048ac1bb882c9183a3f0
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2026-02-05 20:53:59 -0300

    Refactoring utilizando ternário

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function togglePassword() {
     const inputPassword = document.getElementById('password')
     const eyeIcon = document.getElementById('eyeIcon')
 
-    if (inputPassword.type === 'password') {
+    /* if (inputPassword.type === 'password') {
         inputPassword.type = 'text'
         eyeIcon.classList.remove('bi-eye')
         eyeIcon.classList.add('bi-eye-slash')
@@ -12,5 +12,15 @@ function togglePassword() {
         inputPassword.type = 'password'
         eyeIcon.classList.remove('bi-eye-slash')
         eyeIcon.classList.add('bi-eye')
-    }
+    } */
+
+    // constante que retorna true/false, ou seja, é a condição do ternário
+    const ehSenha = inputPassword.type === 'password';
+
+    //Condições utilizando ternário
+    inputPassword.type = ehSenha ? 'text' : 'password'
+    eyeIcon.classList.remove(ehSenha ? 'bi-eye' : 'bi-eye-slash')
+    eyeIcon.classList.add(ehSenha ? 'bi-eye-slash' : 'bi-eye')
+
+
 }
